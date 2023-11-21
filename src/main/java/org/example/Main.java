840ac43b888b24a3d2aac197ff9bfef3fb56e5ef
G00414253@atu.ie <g00414253@atu.ie>
2023-11-21 14:13:57 +0000

    added writetofile class adn added text to a file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,24 +3,25 @@ package org.example;
 import java.io.File;
 import java.io.IOException;
 
-    class CreateFile {
-        public static void main(String[] args) {
+ class CreateFile {
 
-            //Specify the file name
-            String fileName = "File.14.txt";
+    public static void main(String[] args) {
 
-            File myFile = new File(fileName);
+        //Specify the file name
+        String fileName ="File.13.txt";
 
-            try {
-                //Create a new file onb the disk
-                if (myFile.createNewFile()) {
-                    System.out.println("File created successfully at " + myFile.getAbsolutePath());
-                } else {
-                    System.out.println("File already exists at " + myFile.getAbsolutePath());
-                }
-            } catch (IOException e) {
-                System.out.println("An error occurred while creating the file");
-                e.printStackTrace();
+        File myFile = new File(fileName);
+
+        try {
+            //Create a new file onb the disk
+            if (myFile.createNewFile()) {
+                System.out.println("File created successfully at " + myFile.getAbsolutePath());
+            } else {
+                System.out.println("File already exists at " + myFile.getAbsolutePath());
             }
+        }catch(IOException e){
+            System.out.println("An error occurred while creating the file");
+            e.printStackTrace();
         }
     }
+}
