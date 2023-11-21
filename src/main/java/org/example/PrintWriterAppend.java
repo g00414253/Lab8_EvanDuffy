244c68dc2bbbf48da0e936395e84018e7b52c8ac
G00414253@atu.ie <g00414253@atu.ie>
2023-11-21 14:40:29 +0000

    added writetofile class adn added text to a file

---
+++ src/main/java/org/example/PrintWriterAppend.java
@@ -0,0 +1,15 @@
+package org.example;
+import java.io.PrintWriter;
+
+public class PrintWriterAppend {
+    public static void main(String[] args) {
+        String fileName;
+
+
+        try (PrintWriter writer) {
+
+        } catch () {
+
+        }
+    }
+}
